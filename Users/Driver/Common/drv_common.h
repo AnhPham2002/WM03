681aa0ea86b94c4415eba23b995c8fee26af3ffe
Phạm Tuấn Anh <anhpham1712002@gmail.com>
2026-08-18 15:11:08 +0700

    Change driver common to system common

--- Users/Driver/Common/drv_common.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "stm32l4xx_hal.h"
-
-static inline void delay_ms(uint32_t u32Delay)
-{
-    HAL_Delay(u32Delay);
-}
--- Users/Main/app_main.c
+++ Users/Main/app_main.c
@@ -1,5 +1,5 @@
 #include "app_main.h"
-#include "drv_common.h"
+#include "sys_common.h"
 #include "drv_led.h"
 #include "drv_wdt.h"
 
@@ -12,6 +12,6 @@ void app_main(void)
         drv_wdr_restart();
 
         drv_led_blink();
-        delay_ms(1000);
+        sys_delay_ms(1000);
     }
 }
