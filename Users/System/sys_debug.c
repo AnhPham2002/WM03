feedb4ec7d5cf1c322265b0b0a5911bd6edb730f
Phạm Tuấn Anh <anhpham1712002@gmail.com>
2026-08-19 11:25:36 +0700

    Update system debug lib

--- Users/System/sys_debug.c
+++ Users/System/sys_debug.c
@@ -1,5 +1,14 @@
 #include "sys_debug.h"
 
+void sys_debug_init(void)
+{
+#ifdef USE_DEBUG
+    drv_uart2_init(500000, UART_PARITY_NONE);
+#else
+    drv_uart2_deinit();
+#endif
+}
+
 void sys_log(const uint8_t *pData, uint16_t u16Size)
 {
 #ifdef USE_DEBUG
