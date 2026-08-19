72e2215d90723ca1ce5d33354e050b7a8c102e5d
Phạm Tuấn Anh <anhpham1712002@gmail.com>
2026-08-19 13:16:04 +0700

    Add echo in debug console and add doxygen for debug

--- Users/System/sys_debug.h
+++ Users/System/sys_debug.h
@@ -3,6 +3,25 @@
 #include "setting.h"
 #include "drv_uart.h"
 
+/**
+ * @brief Initialize system debug.
+ */
 void sys_debug_init(void);
+
+/**
+ * @brief Send debug data.
+ *
+ * @param[in] pData   Data buffer.
+ * @param[in] u16Size Data size.
+ */
 void sys_log(const uint8_t *pData, uint16_t u16Size);
+
+/**
+ * @brief Receive data from system console.
+ *
+ * @param[out] pData     Receive buffer.
+ * @param[in,out] u16Size Data size.
+ *
+ * @return true if data is received successfully, otherwise false.
+ */
 bool sys_console(uint8_t *pData, uint16_t *u16Size);
