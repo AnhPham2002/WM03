f14a45f00a6c422f71a78299f332846e3f7cc6c8
Phạm Tuấn Anh <anhpham1712002@gmail.com>
2026-08-19 11:25:36 +0700

    Update system debug lib

--- Users/System/sys_debug.h
+++ Users/System/sys_debug.h
@@ -3,5 +3,6 @@
 #include "setting.h"
 #include "drv_uart.h"
 
+void sys_debug_init(void);
 void sys_log(const uint8_t *pData, uint16_t u16Size);
 bool sys_console(uint8_t *pData, uint16_t *u16Size);
