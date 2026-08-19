5f09f1a8c00ed0e953a65268b38a275b1d882eaa
Phạm Tuấn Anh <anhpham1712002@gmail.com>
2026-08-19 15:16:07 +0700

    Add doxygen for wdt driver

--- Users/Driver/External/drv_wdt.h
+++ Users/Driver/External/drv_wdt.h
@@ -4,5 +4,12 @@
 
 #define WDI_PIN GPIOC, GPIO_PIN_13
 
+/**
+ * @brief Initialize watchdog timer.
+ */
 void drv_wdt_init(void);
+
+/**
+ * @brief Restart watchdog timer.
+ */
 void drv_wdr_restart(void);
