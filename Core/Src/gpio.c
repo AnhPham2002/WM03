05a4dcf465a1188fc92aafd74eb6aac7fb8e27b9
Phạm Tuấn Anh <anhpham1712002@gmail.com>
2026-08-21 09:29:19 +0700

    Add External ADC Driver

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -48,9 +48,10 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, WDI_Pin|PWR_4G_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, WDI_Pin|PWR_4G_Pin|EN_SS2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, SPI_CS_EE_Pin|LED_Pin, GPIO_PIN_SET);
@@ -61,8 +62,14 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(PWR_485_GPIO_Port, PWR_485_Pin, GPIO_PIN_SET);
 
-  /*Configure GPIO pins : WDI_Pin PWR_4G_Pin PWR_485_Pin */
-  GPIO_InitStruct.Pin = WDI_Pin|PWR_4G_Pin|PWR_485_Pin;
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(EN_SS1_GPIO_Port, EN_SS1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, PWR_SS_Pin|PWR_E_ADC_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : WDI_Pin PWR_4G_Pin PWR_485_Pin EN_SS2_Pin */
+  GPIO_InitStruct.Pin = WDI_Pin|PWR_4G_Pin|PWR_485_Pin|EN_SS2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -75,12 +82,21 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : KEY_4G_Pin RST_4G_Pin DE_485_Pin */
-  GPIO_InitStruct.Pin = KEY_4G_Pin|RST_4G_Pin|DE_485_Pin;
+  /*Configure GPIO pins : KEY_4G_Pin RST_4G_Pin PWR_SS_Pin DE_485_Pin
+                           PWR_E_ADC_Pin */
+  GPIO_InitStruct.Pin = KEY_4G_Pin|RST_4G_Pin|PWR_SS_Pin|DE_485_Pin
+                          |PWR_E_ADC_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : EN_SS1_Pin */
+  GPIO_InitStruct.Pin = EN_SS1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(EN_SS1_GPIO_Port, &GPIO_InitStruct);
 
 }
 
