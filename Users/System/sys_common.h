4e340702f48195c901281c12cc3a8c3ab3b12786
Phạm Tuấn Anh <anhpham1712002@gmail.com>
2026-08-21 14:04:55 +0700

    Add sleep function

--- Users/System/sys_common.h
+++ Users/System/sys_common.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "stm32l4xx_hal.h"
+#include "stm32l4xx_hal_pwr.h"
+#include "stm32l4xx_hal_pwr_ex.h"
 #include <stdint.h>
 
 static inline void sys_delay_ms(uint32_t u32Delay)
@@ -11,4 +13,11 @@ static inline void sys_delay_ms(uint32_t u32Delay)
 static inline uint32_t sys_time_ms(void)
 {
     return HAL_GetTick();
+}
+
+static inline void sys_sleep(void)
+{
+    HAL_SuspendTick();
+    HAL_PWREx_EnterSTOP2Mode(PWR_STOPENTRY_WFI);
+    HAL_ResumeTick();
 }
