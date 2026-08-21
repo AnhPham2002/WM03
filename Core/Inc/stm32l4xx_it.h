4deed200059e1cece3e06914fc2c0272a61702bc
Phạm Tuấn Anh <anhpham1712002@gmail.com>
2026-08-21 10:30:08 +0700

    Add RTC Driver

--- Core/Inc/stm32l4xx_it.h
+++ Core/Inc/stm32l4xx_it.h
@@ -55,6 +55,7 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
+void RTC_WKUP_IRQHandler(void);
 void DMA1_Channel2_IRQHandler(void);
 void DMA1_Channel3_IRQHandler(void);
 void DMA1_Channel4_IRQHandler(void);
@@ -64,6 +65,7 @@ void DMA1_Channel7_IRQHandler(void);
 void USART1_IRQHandler(void);
 void USART2_IRQHandler(void);
 void USART3_IRQHandler(void);
+void RTC_Alarm_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
---
+++ Users/Driver/Internal/drv_rtc.h
@@ -0,0 +1,6 @@
+#pragma once
+
+/**
+ * @brief Initialize SPI.
+ */
+void drv_rtc_init(void);
