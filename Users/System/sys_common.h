76c24f419c54d848942f5b83145ee486deaf73de
Phạm Tuấn Anh <anhpham1712002@gmail.com>
2026-09-03 11:25:33 +0700

    Add cellular service

--- Users/System/sys_common.h
+++ Users/System/sys_common.h
@@ -7,7 +7,15 @@
 #include "flash_layout.h"
 #include "ram_noinit_layout.h"
 
+#include <math.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#define CLAMP(x, l, h) ((x) < (l) ? (l) : ((x) > (h) ? (h) : (x)))
 
 static inline void sys_delay_ms(uint32_t u32Delay)
 {
