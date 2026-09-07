249b2d9306057e22b2f8cb8718ca4c7974f4abc4
Phạm Tuấn Anh <anhpham1712002@gmail.com>
2026-09-07 09:45:01 +0700

    Add FLASH service

---
+++ Users/Service/sv_flash.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "drv_flash.h"
+
+/**
+ * @brief Erase flash memory.
+ *
+ * @param[in] u32Address Start address.
+ * @param[in] u32Size    Size to erase.
+ *
+ * @return true if successful, otherwise false.
+ */
+bool sv_flash_erase(uint32_t u32Address, uint32_t u32Size);
+
+/**
+ * @brief Write data to flash memory.
+ *
+ * @param[in] u32Address Start address.
+ * @param[in] pData      Data buffer.
+ * @param[in] u32Size    Data size.
+ *
+ * @return true if successful, otherwise false.
+ */
+bool sv_flash_write(uint32_t u32Address, const uint8_t *pData, uint32_t u32Size);
+
+/**
+ * @brief Read data from flash memory.
+ *
+ * @param[in]  u32Address Start address.
+ * @param[out] pData      Data buffer.
+ * @param[in]  u32Size    Data size.
+ */
+void sv_flash_read(uint32_t u32Address, uint8_t *pData, uint32_t u32Size);
+
+/**
+ * @brief Calculate CRC32 of flash data.
+ *
+ * @param[in] u32Address Start address.
+ * @param[in] u32Size    Data size.
+ *
+ * @return CRC32 value.
+ */
+uint32_t sv_flash_crc32(uint32_t u32Address, uint32_t u32Size);
